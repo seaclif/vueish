10bee0a55f42718d03577f37772fdb3c7e91ce97
seaclif <austen.schultz@gmail.com>
2024-01-19 11:22:35 -0700

    VueishHandler

--- vueish.go
+++ vueish.go
@@ -1,4 +1,15 @@
 package vueish
 
-func Hello() {
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+)
+
+//go:embed vueish.js
+var Vueish string
+
+func VueishHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/javascript")
+	fmt.Fprintf(w, Vueish)
 }
